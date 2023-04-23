70cc2e75cda4eeeb2a7839ec861a9270bf8bc37d
kosmonafte <itrzn@mail.ru>
2023-04-23 21:08:30 +0300

    humanClass

---
+++ 27.04.23_4/dz27.04.23/Human.h
@@ -0,0 +1,84 @@
+#pragma once
+#include <string.h>
+#include <iostream>
+class Human
+{
+private:
+	char* name;
+	bool man;
+	char telNumber[13];
+public:
+	Human( const char* name, bool man, const char telNumber[]) {
+		this->name = new char[strlen(name) + 1];
+		for (int i = 0; i < strlen(name); i++) {
+			this->name[i] = name[i];
+		}
+		this->name[strlen(name)] = '\0';
+		this->man = man;
+		if (strlen(telNumber) == 12) {
+			for (int i = 0; i < 12; i++) {
+				this->telNumber[i] = telNumber[i];
+			}
+			this->telNumber[12] = '\0';
+		}
+		else {
+			for (int i = 0; i < 12; i++) {
+				this->telNumber[i] = '*';
+			}
+			std::cout << "Телефон введен не корректно";
+		}
+	}
+	Human(char* name, bool man) :Human(name, man, "************") {};
+	Human(char* name) :Human(name, true, "************") {};
+	Human() :Human("Tester", true, "************") {};
+	Human(const Human &human) {
+		this->name = new char[strlen(human.name) + 1];
+		for (int i = 0; i < strlen(human.name); i++) {
+			this->name[i] = human.name[i];
+		}
+		this->name[strlen(human.name)] = '\0';
+		this->man = human.man;
+		for (int i = 0; i < 12; i++) {
+			this->telNumber[i] = human.telNumber[i];
+		}
+		this->telNumber[12] = '\0';
+	}
+	~Human() {
+		if (this->name) {
+			delete[]this->name;
+		}
+	}
+	void setName(const char* name) {
+		delete[]this->name;
+		this->name = new char[strlen(name) + 1];
+		for (int i = 0; i < strlen(name); i++) {
+			this->name[i] = name[i];
+		}
+		this->name[strlen(name)] = '\0';
+	}
+	void setMan(bool man) {
+		this->man = man;
+	}
+	int setTelNumber(const char tel[]) {
+		if (strlen(tel) == 12) {
+			for (int i = 0; i < 12; i++) {
+				this->telNumber[i] = tel[i];
+			}
+			this->telNumber[12] = '\0';
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	char* getName() {
+		return this->name;
+	}
+	bool getMan() {
+		return this->man;
+	}
+	char* getTelNumber() {
+		return this->telNumber;
+	}
+};
+
---
+++ 27.04.23_4/dz27.04.23/dz27.04.23.cpp
@@ -0,0 +1,47 @@
+﻿// dz27.04.23.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Human.h"
+using namespace std;
+int main()
+{
+	Human hum("Sasha", true, "+79991112233");
+	cout << hum.getName() << endl;
+	cout << hum.getMan() << endl;
+	cout << hum.getTelNumber() << endl;
+	cout << "--------------------------" << endl;
+	Human hum2(hum);
+	Human hum3 = hum;
+	hum.setName("SashaSasha");
+	hum.setTelNumber("+71234567890");
+	cout << hum.getName() << endl;
+	cout << hum.getMan() << endl;
+	cout << hum.getTelNumber() << endl;
+	cout << "--------------------------" << endl;
+	cout << hum2.getName() << endl;
+	cout << hum2.getMan() << endl;
+	cout << hum2.getTelNumber() << endl;
+	char* name2 = new char[100];
+	cin >> name2;
+	hum2.setName(name2);
+	cout << "--------------------------" << endl;
+	cout << hum2.getName() << endl;
+	cout << hum2.getMan() << endl;
+	cout << hum2.getTelNumber() << endl;
+	cout << "--------------------------" << endl;
+	cout << hum3.getName() << endl;
+	cout << hum3.getMan() << endl;
+	cout << hum3.getTelNumber() << endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
